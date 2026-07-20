463007aaf7df56dd1d7911447d51c09448a4031b
7hayatshah <169062351+7hayatshah@users.noreply.github.com>
2026-07-20 18:43:27 +0500

    Split data into training and testing sets

--- main.py
+++ main.py
@@ -41,3 +41,9 @@ df['Type'] = df['Type'].fillna(df['Type'].mode()[0])
 df = pd.get_dummies(df, columns=['Regionname', 'Type'], drop_first=True)
 
 print("✅ Data cleaned!")
+
+#  5: Splitting target 
+X = df.drop(target, axis=1)
+y = df[target]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
